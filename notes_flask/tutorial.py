ebdc696c0128a32bf5e76f0fa680638d8be6f632
Phatomcat <marshall.white@ucas-edu.net>
2025-12-02 14:42:06 -0700

    DONE

---
+++ notes_flask/tutorial.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template,request,redirect,url_for
+
+app = Flask(__name__)
+
+@app.route("/",methods={"POST","GET"})
+def home():
+    if request.method == "POST":
+        return redirect(url_for("user",name=request.form["name"]))
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return"<p>SUP</p>"
+
+@app.route("/<name>")
+def user(name):
+    return f"<h1>Hi {name}!</h1>"
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
